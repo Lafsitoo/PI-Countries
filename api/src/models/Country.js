54b87df4e0983de3698014aaa18301f99af7c181
Lafsitoo <fretteleandro@live.com>
2022-12-21 12:10:49 -0300

    21/12-12:10

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -6,9 +6,45 @@ module.exports = (sequelize) => {
   sequelize.define(
     "country",
     {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: DataTypes.UUIDV4,
+      },
       name: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
+      },
+      flags: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      continent: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      capital: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      subregion: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      area: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      population: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      createdInDb: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
       },
     },
     { timestamps: false }
